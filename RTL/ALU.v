3fc9a927d5779808add8462a585a35e57dff2ffa
yuvraj-s420 <yuvrajsandhu6420@gmail.com>
2025-11-18 04:07:31 -0500

    Implemented most alu_OP cases

--- RTL/ALU.v
+++ RTL/ALU.v
@@ -9,7 +9,32 @@ module ALU(
     output sign_f
 )
 
+always @ (posedge clk) begin
+    
+    /* 
+    Executes instruction based on the ALU's input OPCODE. This opcode is different from the opcode 
+    in the original instruction, and is determined by the control unit once the instruction is fetched
+    */
+    case (alu_OP)
+        3'b000: 
+        3'b001: out = in1 + in2;
+        3'b010: out = in1 - in2;
+        3'b011: out = ~in1;
+        3'b100: out = in1 && in2;
+        3'b101: out = in1 | in2;
 
+        // For logical shifts, 1 bit shift would be easiest to implement with a shift register
+        // For multiple, design at gate level would be more complicated.
+        // Multiple cycles? (want to avoid this). Or, a more sophisticated shifting arrangement. (research)
+        3'b110: out = in1 << ____;          
+        3'b111: out = in1 >> ____;
+    endcase
+
+    // Set flags based on the ALU output data
+    // Where are these flags going to be used?
+
+
+end
 
 
 endmodule
