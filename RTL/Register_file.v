9a76b65c55468dbb0d51a5eb315e77a00f3e2904
yuvraj-s420 <yuvrajsandhu6420@gmail.com>
2025-11-24 18:27:45 -0500

    Added ports to register file

--- RTL/Register_file.v
+++ RTL/Register_file.v
@@ -1,5 +1,24 @@
 module Register_file(
+
+    // Clock signal
+    input clk,
+
+    // Addresses
+    input [2:0] Rd_address,
+    input [2:0] Rs1_address,
+    input [2:0] Rs2_address,
     
+    // Register write enable
+    input reg_w,
+
+    // Data to writeback to register if write enabled
+    input [15:0] writeback_data,
+
+    // Output data
+    output reg [15:0] out_1,
+    output reg [15:0] out_2
 );
-    
+
+
+
 endmodule
