4a57774236b5c95f78a04b8a262b213121f04e1f
yuvraj-s420 <yuvrajsandhu6420@gmail.com>
2025-11-24 19:23:41 -0500

    Implemented reading and writing to RAM memory file

---
+++ RTL/Memory_file.v
@@ -0,0 +1,40 @@
+module Memory_file (
+    
+    // Clock
+    input clk,
+
+    // read/write address (9 bit)
+    input [8:0] addr,
+
+    // Write data
+    input [15:0] write_data,
+
+    // Control signals
+    input mem_r,
+    input mem_w,
+
+    // Data out
+    output reg [15:0] out
+);
+
+reg [15:0] mem [511:0]      // RAM consisting of 512 locations of 16 bit words
+
+// Synchronous writing 
+always @(posedge clk) begin
+
+    if (mem_w) begin
+        mem[addr] = write_data;
+    end
+
+end
+
+// Asynchronous reading
+always @* begin
+
+    if (mem_r) begin
+        out = mem[addr];
+    end
+
+end
+
+endmodule
