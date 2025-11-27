bafc2ed742e5a994f7bbd8cd4609cf0d4bbafc06
yuvraj-s420 <yuvrajsandhu6420@gmail.com>
2025-11-27 15:34:42 -0500

    Implemented writeback mux cases with set to 0 safety for unused select case

--- RTL/Writeback_MUX.v
+++ RTL/Writeback_MUX.v
@@ -1,7 +1,7 @@
 module Writeback_MUX (
     
     // MUX inputs
-    input [15:0] alu_out,
+    input [15:0] alu_out,           
     input [8:0] imm,
     input [15:0] ram_out,
 
@@ -13,4 +13,18 @@ module Writeback_MUX (
 
 );
     
+// Asynchronous updating
+always @* begin
+    case (reg_w_from)                   // Select which data line to send to Register_file
+
+        2'b00: out = alu_out;           // ADD, ADDI, SUB, SUBI, AND, OR, NOT, LSL, LSR
+        2'b01: out = ram_out;           // LOAD
+        2'b10: begin                    // LOADI
+            out = {7{imm[8]}, imm};     // Sign extend immediate to 16 bits 
+        end
+        2'b11: out = 16'h0000;          // Unused, set out to 0 for safety
+        
+    endcase
+end
+
 endmodule
